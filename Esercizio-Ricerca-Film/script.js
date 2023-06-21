9babf88a6ecefbc953b7c90fa65f5f877c739cd4
Vladyslav Okopnyy <harmfulcati@gmail.com>
2023-06-21 13:58:21 +0200

    Fix

--- Esercizio-Ricerca-Film/script.js
+++ Esercizio-Ricerca-Film/script.js
@@ -1,15 +1,15 @@
 angular.module('movieSearchApp', [])
 .controller('movieSearchController', function ($scope, $http) {
   $scope.searchQuery = '';
-  $scope.movies;
+  $scope.movie;
 
   $scope.searchMovies = function () {
     var apiUrl = 'https://www.omdbapi.com/?apikey=81141edd&t=' + $scope.searchQuery;
 
     $http.get(apiUrl)
       .then(function (response) {
-        $scope.movies = response.data;
-        console.log($scope.movies);
+        $scope.movie = response.data;
+        console.log($scope.movie);
       })
       .catch(function (error) {
         console.log('Error:', error);
